0368f1b78bf6edfe7ccc212e4e83f439a1404de7
Ak9312 <129245931+Ak9312@users.noreply.github.com>
2024-04-19 12:53:11 +0530

    Added Local Storage and Countdown Interval

--- Chapter 14/CountDown/CountDown.js
+++ Chapter 14/CountDown/CountDown.js
@@ -1,28 +1,42 @@
-var date = document.querySelector(".Date");
-var clocks = document.querySelector(".clock");
-var day = document.querySelector(".days");
-var hour = document.querySelector(".hours");
-var minute = document.querySelector(".minutes");
-var second = document.querySelector(".seconds");
-let timeInterval;
-var timeStop = true;
-var getDate = document.querySelector(".GetDate");
+let date = document.querySelector(".Date");
+let clocks = document.querySelector(".clock");
+let day = document.querySelector(".days");
+let hour = document.querySelector(".hours");
+let minute = document.querySelector(".minutes");
+let second = document.querySelector(".seconds");
 
-let saved = localStorage.getItem("countdown");
-getDate.addEventListener("click", () => {
-  selectedDate = new Date(date.value).getTime();
-  finalDate = new Date().getTime();
-  totalTime = finalDate - selectedDate;
+let savedInitialValue = localStorage.getItem("initial");
+let savedCountDown = localStorage.getItem("countdown");
 
+if (savedInitialValue != null) {
+  date.value = savedInitialValue;
+}
 
-    var days = Math.floor(totalTime / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((totalTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((totalTime % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((totalTime % (1000 * 60)) / 1000);
+if (savedCountDown != null) {
+  countdownStart(savedCountDown);
+}
 
- 
-    day.textContent = days;
-    hour.textContent = hours;
-    minute.textContent = minutes;
-    second.textContent = seconds;
+window.addEventListener("load", () => {
+  setInterval(() => {
+    let initialDate = date.value;
+    let selectedDate = new Date(initialDate).getTime();
+    localStorage.setItem("initial", initialDate);
+    finalDate = new Date().getTime();
+    totalTime = selectedDate - finalDate;
+    localStorage.setItem("countdown", totalTime);
+
+    countdownStart(totalTime);
+  }, 1000);
 });
+
+function countdownStart(value) {
+  let days = Math.floor(value / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((value % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((value % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((value % (1000 * 60)) / 1000);
+
+  day.textContent = days;
+  hour.textContent = hours;
+  minute.textContent = minutes;
+  second.textContent = seconds;
+}
